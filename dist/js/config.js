aceb7c8a126fba7d9cbc5f776e7d2ab885cf5968
Xiaoyu Zhong <xiaoyu.zhong@booking.com>
2022-07-13 11:04:15 +0800

    Move to /dist

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
-  redirectUrl: `https://bittopaz.github.io/on-call-handover`,
+  redirectUrl: `https://bittopaz.github.io/on-call-handover/dist`,
   clientId: `23989305-543e-4891-86e5-c9b60e307482`,
 };
